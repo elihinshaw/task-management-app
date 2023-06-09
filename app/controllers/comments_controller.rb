2af8b7331f6800b9fba9afffbfce1b7e1a6a930a
Eli <eghinshaw@gmail.com>
2023-06-08 21:36:21 -0500

    added crud operations to controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,44 @@ class CommentsController < ApplicationController
     @comments = Comment.all
     render :index
   end
+
+  def show
+    @comment = Comment.find_by(id: params[:id])
+    render :show
+  end
+
+  def new
+    @comment = Comment.new
+    render :new
+  end
+
+  def create
+    @comment = Comment.create(
+      content: params[:comment][:content],
+      user_id: params[:comment][:user_id],
+      task_id: params[:comment][:task_id],
+    )
+    redirect_to "/comments"
+  end
+
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    @comment.update(
+      content: params[:comment][:content],
+      user_id: params[:comment][:user_id],
+      task_id: params[:comment][:task_id],
+    )
+    redirect_to "/comments"
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    redirect_to "/comments", status: :see_other
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,52 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Tasks.all
+    render :index
+  end
+
+  def show
+    @tasks = Tasks.find_by(id: params[:id])
+    render :show
+  end
+
+  def new
+    @tasks = Tasks.new
+    render :new
+  end
+
+  def create
+    @tasks = Tasks.create(
+      name: params[:tasks][:name],
+      description: params[:tasks][:description],
+      priority: params[:tasks][:priority],
+      due_date: params[:tasks][:due_date],
+      status: params[:tasks][:status],
+      progress: params[:tasks][:progress],
+    )
+    redirect_to "/tasks"
+  end
+
+  def edit
+    @tasks = Tasks.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @tasks = Tasks.find_by(id: params[:id])
+    @tasks.update(
+      name: params[:tasks][:name],
+      description: params[:tasks][:description],
+      priority: params[:tasks][:priority],
+      due_date: params[:tasks][:due_date],
+      status: params[:tasks][:status],
+      progress: params[:tasks][:progress],
+    )
+    redirect_to "/tasks"
+  end
+
+  def destroy
+    @tasks = Tasks.find_by(id: params[:id])
+    @tasks.destroy
+    redirect_to "/tasks", status: :see_other
+  end
 end
